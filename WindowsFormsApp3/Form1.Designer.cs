9d5d548ee9feb6955f75bc5660f071fdedd33a50
Thanh123lxag <131396738+Thanh123lxag@users.noreply.github.com>
2023-04-21 01:17:05 +0700

    Add files via upload

---
+++ WindowsFormsApp3/Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace WindowsFormsApp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.comboBoxSoup = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(333, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nhà hàng ABC";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(64, 65);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Lẩu";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(64, 122);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(61, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Thịt thêm";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(64, 183);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(64, 16);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Rau thêm";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(64, 243);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(58, 16);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Xí quách";
+            // 
+            // comboBoxSoup
+            // 
+            this.comboBoxSoup.FormattingEnabled = true;
+            this.comboBoxSoup.Location = new System.Drawing.Point(228, 65);
+            this.comboBoxSoup.Name = "comboBoxSoup";
+            this.comboBoxSoup.Size = new System.Drawing.Size(121, 24);
+            this.comboBoxSoup.TabIndex = 5;
+            this.comboBoxSoup.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.AccessibleName = "textBoxMeat";
+            this.textBox1.Location = new System.Drawing.Point(228, 122);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 6;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(228, 176);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 7;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(228, 240);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 22);
+            this.textBox3.TabIndex = 8;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(599, 65);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Tính tiền";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AccessibleDescription = "";
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(537, 151);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(240, 150);
+            this.dataGridView1.TabIndex = 10;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.comboBoxSoup);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox comboBoxSoup;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ WindowsFormsApp3/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        // Khai báo danh sách các loại lẩu và giá tiền
+        private Dictionary<string, int> soupList = new Dictionary<string, int>()
+    {
+        {"Lẩu thái", 100000},
+        {"Lẩu Hàn Quốc", 120000},
+        {"Lẩu Sichuan", 150000},
+        {"Lẩu nấm", 90000},
+    };
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Thêm danh sách các loại lẩu vào ComboBox
+            foreach (var soup in soupList)
+            {
+                comboBoxSoup.Items.Add(soup.Key + " - " + soup.Value.ToString("N0") + " VNĐ");
+            }
+
+            // Thiết lập giao diện DataGridView
+            dataGridViewBill.Columns.Add("colItem", "Món ăn");
+            dataGridViewBill.Columns.Add("colQuantity", "Số lượng");
+            dataGridViewBill.Columns.Add("colPrice", "Đơn giá");
+            dataGridViewBill.Columns.Add("colTotal", "Thành tiền");
+
+            dataGridViewBill.Columns["colItem"].Width = 200;
+            dataGridViewBill.Columns["colQuantity"].Width = 70;
+            dataGridViewBill.Columns["colPrice"].Width = 100;
+            dataGridViewBill.Columns["colTotal"].Width = 120;
+
+            dataGridViewBill.Columns["colPrice"].DefaultCellStyle.Format = "N0";
+            dataGridViewBill.Columns["colTotal"].DefaultCellStyle.Format = "N0";
+        }
+
+        private void buttonCalculate_Click(object sender, EventArgs e)
+        {
+            // Lấy thông tin lẩu được chọn từ ComboBox
+            string selectedSoup = comboBoxSoup.SelectedItem.ToString().Split('-')[0].Trim();
+            int soupPrice = soupList[selectedSoup];
+
+            // Lấy thông tin số lượng thịt, rau, xí quách
+            int meatQuantity = Convert.ToInt32(textBoxMeat.Text);
+            int vegQuantity = Convert.ToInt32(textBoxVeg.Text);
+            int xiquachQuantity = Convert.ToInt32(textBoxXiquach.Text);
+
+            // Tính tổng tiền hóa đơn
+            int totalPrice = soupPrice + meatQuantity * 50000 + vegQuantity * 20000 + xiquachQuantity * 15000;
+
+            // Hiển thị thông tin hóa đơn trên DataGridView
+            dataGridViewBill.Rows.Add(selectedSoup, meatQuantity + " phần", "50,000", meatQuantity * 50000);
+            dataGridViewBill.Rows.Add("Rau thêm", vegQuantity + " phần", "20,000", vegQuantity * 20000);
+            dataGridViewBill.Rows.Add("Xí quách", xiquachQuantity + " phần", "15,000", xiquachQuantity * 15000);
+
+            // Hiển thị tổng tiền hóa đơn
+            labelTotal.Text = totalPrice.ToString("N0") + " VNĐ";
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+
+}
