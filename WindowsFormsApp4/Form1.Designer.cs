8e9d6d38a2572df694886638eae2f1304246a874
Thanh123lxag <131396738+Thanh123lxag@users.noreply.github.com>
2023-04-23 18:50:31 +0700

    Add files via upload

---
+++ WindowsFormsApp4/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace QuanLyChamCong
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtTen = new System.Windows.Forms.TextBox();
+            this.txtMaNV = new System.Windows.Forms.TextBox();
+            this.dtpGioVao = new System.Windows.Forms.DateTimePicker();
+            this.dtpGioRa = new System.Windows.Forms.DateTimePicker();
+            this.btnThem = new System.Windows.Forms.Button();
+            this.btnTinhLuong = new System.Windows.Forms.Button();
+            this.lvwDanhSachNV = new System.Windows.Forms.ListView();
+            this.SuspendLayout();
+            // 
+            // txtTen
+            // 
+            this.txtTen.Location = new System.Drawing.Point(150, 43);
+            this.txtTen.Name = "txtTen";
+            this.txtTen.Size = new System.Drawing.Size(100, 22);
+            this.txtTen.TabIndex = 4;
+            this.txtTen.TextChanged += new System.EventHandler(this.txtTen_TextChanged);
+            // 
+            // txtMaNV
+            // 
+            this.txtMaNV.Location = new System.Drawing.Point(150, 125);
+            this.txtMaNV.Name = "txtMaNV";
+            this.txtMaNV.Size = new System.Drawing.Size(100, 22);
+            this.txtMaNV.TabIndex = 5;
+            // 
+            // dtpGioVao
+            // 
+            this.dtpGioVao.Location = new System.Drawing.Point(112, 199);
+            this.dtpGioVao.Name = "dtpGioVao";
+            this.dtpGioVao.Size = new System.Drawing.Size(200, 22);
+            this.dtpGioVao.TabIndex = 6;
+            // 
+            // dtpGioRa
+            // 
+            this.dtpGioRa.Location = new System.Drawing.Point(112, 276);
+            this.dtpGioRa.Name = "dtpGioRa";
+            this.dtpGioRa.Size = new System.Drawing.Size(200, 22);
+            this.dtpGioRa.TabIndex = 7;
+            // 
+            // btnThem
+            // 
+            this.btnThem.Location = new System.Drawing.Point(112, 366);
+            this.btnThem.Name = "btnThem";
+            this.btnThem.Size = new System.Drawing.Size(75, 23);
+            this.btnThem.TabIndex = 8;
+            this.btnThem.Text = "Thêm lương";
+            this.btnThem.UseVisualStyleBackColor = true;
+            // 
+            // btnTinhLuong
+            // 
+            this.btnTinhLuong.Location = new System.Drawing.Point(237, 366);
+            this.btnTinhLuong.Name = "btnTinhLuong";
+            this.btnTinhLuong.Size = new System.Drawing.Size(75, 23);
+            this.btnTinhLuong.TabIndex = 9;
+            this.btnTinhLuong.Text = "Tính lương";
+            this.btnTinhLuong.UseVisualStyleBackColor = true;
+            // 
+            // lvwDanhSachNV
+            // 
+            this.lvwDanhSachNV.HideSelection = false;
+            this.lvwDanhSachNV.Location = new System.Drawing.Point(490, 199);
+            this.lvwDanhSachNV.Name = "lvwDanhSachNV";
+            this.lvwDanhSachNV.Size = new System.Drawing.Size(121, 97);
+            this.lvwDanhSachNV.TabIndex = 10;
+            this.lvwDanhSachNV.UseCompatibleStateImageBehavior = false;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lvwDanhSachNV);
+            this.Controls.Add(this.btnTinhLuong);
+            this.Controls.Add(this.btnThem);
+            this.Controls.Add(this.dtpGioRa);
+            this.Controls.Add(this.dtpGioVao);
+            this.Controls.Add(this.txtMaNV);
+            this.Controls.Add(this.txtTen);
+            this.Name = "MainForm";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox txtTen;
+        private System.Windows.Forms.TextBox txtMaNV;
+        private System.Windows.Forms.DateTimePicker dtpGioVao;
+        private System.Windows.Forms.DateTimePicker dtpGioRa;
+        private System.Windows.Forms.Button btnThem;
+        private System.Windows.Forms.Button btnTinhLuong;
+        private System.Windows.Forms.ListView lvwDanhSachNV;
+    }
+}
+
---
+++ WindowsFormsApp4/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuanLyChamCong
+{
+    public partial class MainForm : Form
+    {
+        private List<NhanVien> dsNhanVien = new List<NhanVien>();
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnThem_Click(object sender, EventArgs e)
+        {
+            // Lấy thông tin từ các control trên form
+            string ten = txtTen.Text;
+            string maNV = txtMaNV.Text;
+            DateTime gioVao = dtpGioVao.Value;
+            DateTime gioRa = dtpGioRa.Value;
+
+            // Tạo đối tượng NhanVien và thêm vào danh sách
+            NhanVien nv = new NhanVien(ten, maNV, gioVao, gioRa);
+            dsNhanVien.Add(nv);
+
+            // Hiển thị thông tin của nhân viên vừa thêm vào ListView
+            ListViewItem item = new ListViewItem(new string[] { nv.Ten, nv.MaNV, nv.GioVao.ToString(), nv.GioRa.ToString() });
+            lvwDanhSachNV.Items.Add(item);
+        }
+
+        private void btnTinhLuong_Click(object sender, EventArgs e)
+        {
+            // Tính lương cho từng nhân viên và hiển thị kết quả
+            foreach (NhanVien nv in dsNhanVien)
+            {
+                double tongGioLam = (nv.GioRa - nv.GioVao).TotalHours;
+                double luong = tongGioLam * 100000; // Giả sử mỗi giờ làm được trả 100.000 VNĐ
+                MessageBox.Show("Nhân viên " + nv.Ten + " có lương: " + luong.ToString("N0") + " VNĐ");
+            }
+        }
+
+        private void txtTen_TextChanged(object sender, EventArgs e)
+        {
+        }
+    }
+
+    // Định nghĩa class NhanVien để lưu trữ thông tin của nhân viên
+    public class NhanVien
+    {
+        public string Ten { get; set; }
+        public string MaNV { get; set; }
+        public DateTime GioVao { get; set; }
+        public DateTime GioRa { get; set; }
+
+        public NhanVien(string ten, string maNV, DateTime gioVao, DateTime gioRa)
+        {
+            Ten = ten;
+            MaNV = maNV;
+            GioVao = gioVao;
+            GioRa = gioRa;
+        }
+    }
+}
